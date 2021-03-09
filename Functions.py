92472c86fa0e18752cc8d3fc828550de14fea3cc
Jonathan Iuri <JonathanPoli17@Gmail.com>
2021-03-09 17:30:50 -0300

    adicionando código de barra

---
+++ Functions.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+class SQDB(): #Funcoes com o banco de dados
+    def __init__(self):
+        super().__init__()
+
+        #Criando ou abrindo a pasta do banco de dados
+        Pasta = Path.home() / path_join("MyAppPastaJP")
+        try:
+            pastinha = Pasta.mkdir()
+        except:
+            pass
+        self.connection = sqlite3.connect(Pasta / 'AntVencimentApp.db')
+        self.cursor = self.connection.cursor()
+        
+        self.CreateTables()
+    
+    def CreateTables(self):
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS Items(Codigo interger primary key, Codigo_Barras interger, Nome text, Data_Vencimento text)')
