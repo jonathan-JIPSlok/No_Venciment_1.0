51f46c6086f277bb9f6e1882c253a7cefa67a636
Jonathan Iuri <JonathanPoli17@Gmail.com>
2021-03-09 15:53:33 -0300

    Iniciei a janela

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel
+import sqlite3
+import sys
+from Design import *
+from Functions import *
+
+class Primary_Windows(QMainWindow): # Janela Princial
+    def __init__(self):
+        super().__init__()
+        
+        
+        self.show()
+
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    Janela = Primary_Windows()
+    sys.exit(App.exec_())
